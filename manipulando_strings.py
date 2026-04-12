045aa853d3f71948c5dcd616f3e52dfffd388bfd
Daniel_Flagas <128199807+danielsssousa@users.noreply.github.com>
2026-04-11 22:29:38 -0300

    Add files via upload

---
+++ manipulando_strings.py
@@ -0,0 +1,17 @@
+my_name = 'Daniel Lira Flagas Sousa'
+
+print(my_name.upper()) #A utilizacao de upper para caixa alta ou d elower para minusculas.
+print()
+print(my_name[0:6]) #Utilizase tambem o fatiamento.
+
+print('=' * 55)
+
+#my_name = my_name.replace('Lira', 'Araujo') #O replace subistitui um elemento encontrado na string por um novo, faz a substituicao
+#print(my_name)
+
+#lembrando que podemos concatenar varias funcos como:
+
+my_name = my_name.replace('Lira', 'Araujo') #O replace subistitui um elemento encontrado na string por um novo, faz a substituicao
+print(my_name)
+
+print(my_name.split(' '))
