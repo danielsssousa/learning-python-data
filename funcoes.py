1eabcaf80ab92be11910dd72811f0b758e2663b2
Daniel_Flagas <128199807+danielsssousa@users.noreply.github.com>
2026-04-19 17:41:52 -0300

    Add files via upload

--- funcoes.py
+++ funcoes.py
@@ -1,22 +1,6 @@
-#Sintaxe de um parametro: def nome_da_funcao(parametros): codigo return valor
+def nome_completo(nome, sobrenome):
+    print('Nome completo: ', nome, sobrenome)
 
-def soma(a, b):
-    resultado = a + b
-
-    return resultado
-
-def multi(a, b):
-    resultado = a * b
-    return resultado
-
-
-print(f'Resultado {soma(20, 30)}') #Deve ser inserida a funcao nao a variavel
-print(f'Rsultado da multiplicacao {multi(20, 5)}')
-
-
-def ola(nome):
-    saudacao = f'seja bien venindo {nome}'
-    return saudacao
-
-print(ola('Matheus'))
-
+nome_completo('Daniel', 'Lira')
+nome_completo('Emanuela', 'Araujo')
+nome_completo('Maria', 'Gutiana')
