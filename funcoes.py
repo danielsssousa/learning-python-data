d443d2f1bbe1a2592331a5b5a578153e5d674051
Daniel_Flagas <128199807+danielsssousa@users.noreply.github.com>
2026-07-07 23:06:43 -0300

    Delete funcoes.py

--- funcoes.py
+++
@@ -1,6 +0,0 @@
-def nome_completo(nome, sobrenome):
-    print('Nome completo: ', nome, sobrenome)
-
-nome_completo('Daniel', 'Lira')
-nome_completo('Emanuela', 'Araujo')
-nome_completo('Maria', 'Gutiana')
