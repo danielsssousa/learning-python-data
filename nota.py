8fae63338d676fc638beb005bddedecb91386ce2
Daniel_Flagas <128199807+danielsssousa@users.noreply.github.com>
2026-07-20 23:31:25 -0300

    Delete nota.py

--- nota.py
+++
@@ -1,8 +0,0 @@
-pontos = int(input('Digite sua nota: '))
-
-if pontos >= 7:
-    print('Você foi apovado')
-elif pontos >= 5:
-    print('Você foi reprovado')
-else:
-    print('Você está de recuperação')
