c1095abfee6379582b01e0c0889f3fe71a6f9273
Daniel_Flagas <128199807+danielsssousa@users.noreply.github.com>
2026-07-21 12:38:24 -0300

    Add files via upload

---
+++ split_strip_join.py
@@ -0,0 +1,8 @@
+
+pecas = ['ram', 'computador', 'fenda']
+
+texto = ' I lost my key '
+
+print(texto.split()) # Divisor de um texto
+print(texto.strip()) # Limpador de texto. e removedor de caracteres especificos.
+print(', '.join(pecas)) # Ounificador join faz o oposto do split
