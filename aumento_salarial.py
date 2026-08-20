05b60703d50506e2a051aa2b6d337492515af652
Daniel_Flagas <128199807+danielsssousa@users.noreply.github.com>
2026-08-19 22:18:58 -0300

    Delete aumento_salarial.py

--- aumento_salarial.py
+++
@@ -1,9 +0,0 @@
-salario = float(input('Digite o valor de seu soldo atual: R$ '))
-
-if salario <= 1.250:
-    novo = salario + (salario * 15 / 100)
-
-else:
-    novo = salario + (salario * 10 / 100)
-
-print('O seu salario atual é de R${:.2f} reais, com o reajuste de aumento passa a ser R${:.2f} reais!' .format(salario, novo))
