75a906b5aa6ca8fabd48df89e3e56bc0b204ce8c
Daniel_Flagas <128199807+danielsssousa@users.noreply.github.com>
2026-08-20 22:01:32 -0300

    Delete Polimorfismo e Heranca.py

--- Polimorfismo e Heranca.py
+++
@@ -1,9 +0,0 @@
-class Carro:
-    def __init__(self, modelo, ano, cor):
-        self.modelo = modelo
-        self.ano = ano
-        self.cor = cor
-
-carro = Carro("Ford", 2009, "Amarillo")
-
-print(carro.cor)
